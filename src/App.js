9bbdf186e02ed30cfbd23f36f2ed9bb3449a6634
mustafa-bilen <mustafabilen6669@gmail.com>
2022-10-01 16:15:29 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import { Navbar, Input } from "./components/index";
 import React, { useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [location, setLocation] = useState("Ankara");
+
+  const WEATHER_API = process.env.REACT_APP_WEATHER_API;
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${WEATHER_API}`;
+
+  const getInfo = () => {
+    axios
+      .get(URL)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((ERR) => console.log(ERR));
+  };
 
   return (
     <div>
       <Navbar />
-      <Input />
+      <Input getLocation={setLocation} getInfos={getInfo} />
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-const Input = () => {
+const Input = ({ getLocation, getInfos }) => {
   return (
     <div className="flex justify-center items-center">
       <div className="mt-7">
         <input
           placeholder="Enter a city"
           className="px-3 py-2 sm:w-[25rem] bg-slate-300 text-black outline-none rounded-lg focus:bg-slate-200 placeholder:text-slate-500"
+          onChange={(e) => getLocation(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              getInfos();
+            }
+          }}
         />
       </div>
     </div>
