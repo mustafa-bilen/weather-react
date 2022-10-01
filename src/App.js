9bfb166ec7c1e88a2a520dd5c9e02e3734549e88
mustafa-bilen <mustafabilen6669@gmail.com>
2022-10-01 15:32:09 +0300

    env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Navbar, Input } from "./components/index";
+import React, { useState } from "react";
 function App() {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=${WEATHER_API}`;
+
   return (
     <div>
       <Navbar />
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const Input = () => {
   return (
-    <div>
-      <div>
-        <input placeholder="Enter a city" />
+    <div className="flex justify-center items-center">
+      <div className="mt-7">
+        <input
+          placeholder="Enter a city"
+          className="px-3 py-2 sm:w-[25rem] bg-slate-300 text-black outline-none rounded-lg focus:bg-slate-200 placeholder:text-slate-500"
+        />
       </div>
     </div>
   );
