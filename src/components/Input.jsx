47128bc246eb608da637eabf641407908d104416
mustafa-bilen <mustafabilen6669@gmail.com>
2022-10-01 15:24:55 +0300

    weather-react-teamwork

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <div>
+        <input placeholder="Enter a city" />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
